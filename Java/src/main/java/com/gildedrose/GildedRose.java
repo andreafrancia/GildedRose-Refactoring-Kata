fb49490d2fcbb76d044ac685d662d20d6caa54e4
Mattia Panzeri <dev@mattiapanzeri.it>
2024-02-15 19:49:36 +0100

    Lift-Up Conditional
    Co-authored-by: Marco Testa <mrc.tst@gmail.com>

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,50 +13,56 @@ class GildedRose {
         }
     }
 
-    private boolean isPass(Item item)
-    {
+    private boolean isPass(Item item) {
         return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 
     private Item updateItemQuality(Item item) {
         var updatedItem = new Item(item.name, item.sellIn, item.quality);
 
-        if (!updatedItem.name.equals("Aged Brie") && !isPass(updatedItem)) {
-            if (updatedItem.quality > 0 && !updatedItem.name.equals("Sulfuras, Hand of Ragnaros")) {
+        if (!isPass(updatedItem) && !updatedItem.name.equals("Sulfuras, Hand of Ragnaros")
+                && !updatedItem.name.equals("Aged Brie")) {
+            if (updatedItem.quality > 0) {
                 updatedItem.quality = updatedItem.quality - 1;
             }
-        } else {
+            updatedItem.sellIn = updatedItem.sellIn - 1;
+
+            if (updatedItem.sellIn < 0) {
+                if (updatedItem.quality > 0) {
+                    updatedItem.quality = updatedItem.quality - 1;
+                }
+            }
+        }
+
+        if (isPass(updatedItem)) {
+
             if (updatedItem.quality < 50) {
                 updatedItem.quality = updatedItem.quality + 1;
 
-                if (isPass(updatedItem)) {
-                    if (updatedItem.sellIn < 11 && updatedItem.quality < 50) {
-                        updatedItem.quality = updatedItem.quality + 1;
-                    }
-
-                    if (updatedItem.sellIn < 6 && updatedItem.quality < 50) {
-                        updatedItem.quality = updatedItem.quality + 1;
-                    }
+                if (updatedItem.sellIn < 11 && updatedItem.quality < 50) {
+                    updatedItem.quality = updatedItem.quality + 1;
                 }
+
+                if (updatedItem.sellIn < 6 && updatedItem.quality < 50) {
+                    updatedItem.quality = updatedItem.quality + 1;
+                }
+            }
+            updatedItem.sellIn = updatedItem.sellIn - 1;
+
+            if (updatedItem.sellIn < 0) {
+                updatedItem.quality = updatedItem.quality - updatedItem.quality;
             }
         }
 
-        if (!updatedItem.name.equals("Sulfuras, Hand of Ragnaros")) {
-            updatedItem.sellIn = updatedItem.sellIn - 1;
-        }
+        if (updatedItem.name.equals("Aged Brie")) {
 
-        if (updatedItem.sellIn < 0) {
-            if (!updatedItem.name.equals("Aged Brie")) {
-                if (!isPass(updatedItem)) {
-                    if (updatedItem.quality > 0) {
-                        if (!updatedItem.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            updatedItem.quality = updatedItem.quality - 1;
-                        }
-                    }
-                } else {
-                    updatedItem.quality = updatedItem.quality - updatedItem.quality;
-                }
-            } else {
+            if (updatedItem.quality < 50) {
+                updatedItem.quality = updatedItem.quality + 1;
+
+            }
+            updatedItem.sellIn = updatedItem.sellIn - 1;
+
+            if (updatedItem.sellIn < 0) {
                 if (updatedItem.quality < 50) {
                     updatedItem.quality = updatedItem.quality + 1;
                 }
